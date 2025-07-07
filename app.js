31444fa774cca707713bb164586dd58cb928cb90
boriscetkovic1 <boriscetkovic.me@gmail.com>
2025-07-07 23:13:31 +0200

    Connected to MongoDB using Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const connectDatabase = require("./config/db");
 
 const app = express();
+
+connectDatabase();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
